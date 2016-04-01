87cb9c66626db1253bbc146fe413c9ce7d5626af
Jackkell <bronzeeagle1@hotmail.com>
2016-03-31 22:04:36 -0700

    Fixed the register class

--- src/com/github/jackkell/cpuemulator/Main.java
+++ src/com/github/jackkell/cpuemulator/Main.java
@@ -1,21 +1,14 @@
 package com.github.jackkell.cpuemulator;
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.github.jackkell.cpuemulator.cpu.Register;
 
 public class Main {
     public static void main(String[] args) {
-        int source = 0b11111101;
-
-        System.out.println(Integer.toBinaryString(source));
-        System.out.println(Integer.toBinaryString(source & (1<<3)-1));
-        getBits(source, 3, 0);
+        Register register = new Register(5L);
+        System.out.println(Long.toBinaryString(register.get8BitLower()));
+        //System.out.println(register.getBit(0));
+        //System.out.println(register.getBit(1));
+        //System.out.println(register.getBit(2));
+        System.out.println(Integer.toBinaryString(5 >> 2 & 1));
     }
-
-
-    private static void getBits(int source, int bits, int bitoffset) {
-        System.out.println(Integer.toBinaryString((source >> bitoffset) & (1 << bits) - 1));
-    }
-
 }
--- src/com/github/jackkell/cpuemulator/cpu/Register.java
+++ src/com/github/jackkell/cpuemulator/cpu/Register.java
@@ -9,25 +9,12 @@ public class Register {
 
     private Long value;
 
-    public void setBit(int index, int bitValue) {
-        if (bitValue != 0) {
-            value = value | 1 << index;
-        } else {
-            value = value & ~(1 << index);
-        }
-    }
-
-    public void setBits(int newBits, int bitLength, int offset) {
-        int valueLen = Long.toBinaryString(value).length();
-        long savedSourceOffset = 0;
-        if (offset > 0) {
-            savedSourceOffset = Long.parseLong(Long.toBinaryString(value).substring(valueLen - offset, valueLen));
-        }
-        value = (value >> bitLength + offset << bitLength + offset) | (newBits << offset) | savedSourceOffset;
+    public long getBit(int index) {
+        return value >> index & 1;
     }
 
     private long getBits(int numberOfBits, int bitOffset) {
-        return (value >> bitOffset) & ((1 << numberOfBits) - 1);
+        return (value >> bitOffset) & ((1L << numberOfBits) - 1);
     }
 
     private long getBits(int numberOfBits) {
@@ -38,56 +25,50 @@ public class Register {
         return value;
     }
 
-    public void set64Bit(long input) {
-        this.value = input;
-    }
-
     public long get32Bit() {
         return getBits(32);
-    }
-
-    public void set32Bit(int input) {
-        value = value >> 32 << 32 | input;
     }
 
     public long get16Bit() {
         return getBits(16);
     }
 
-    public void set16Bit(short input) {
-        value = value >> 16 << 16 | input;
-    }
-
     public long get8BitLower() {
         return getBits(8);
-    }
-
-    public void set8BitLower(Byte input) {
-        value = value >> 8 << 8 | input;
     }
 
     public long get8BitHigher() {
         return getBits(8, 8);
     }
 
+    public void setBit(int index, int bitValue) {
+        if (bitValue != 0) {
+            value = value | 1 << index;
+        } else {
+            value = value & ~(1 << index);
+        }
+    }
+
+    public void set64Bit(long input) {
+        this.value = input;
+    }
+
+    public void set32Bit(int input) {
+        value = value >> 32 << 32 | input;
+    }
+
+    public void set16Bit(short input) {
+        value = value >> 16 << 16 | input;
+    }
+
+    public void set8BitLower(Byte input) {
+        value = value >> 8 << 8 | input;
+    }
+
     public void set8BitHigher(Byte input) {
         value = (((value >> 16 << 8) | input) << 8) | get8BitLower();
     }
 }
-
-/*
-
-1010 |
-0100 = 1<<2
-1110
-
-1110 &
-1011 = ~(1<<2)
-1010
-
-1011 |
-1101 = 1001 | (0b10<<1)
-*/
 
 
 
